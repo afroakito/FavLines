14f0e5f0c25f3355df37f87148b8b375a601881c
afroakito <akitowens999916@gmail.com>
2019-04-15 02:11:55 +0900

    Create edit-route, edit-controller, edit-view. Add edit- and destroy-button to the show-view.

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,15 @@
 Rails.application.routes.draw do
+    get "login_form" => "users#login_form"
 
-  get "login_form" => "users#login_form"
-  
-  get "/" => "home#top"
-  get "about" => "home#about"
+    get "/" => "home#top"
+    get "about" => "home#about"
 
-  get "posts/index" => "posts#index"
-  get "posts/:id" => "posts#show"
-  get "posts/new" => "posts#new"
-  post "posts/create" => "posts#create"
+    get "posts/index" => "posts#index"
+    get "posts/new" => "posts#new"
+    post "posts/create" => "posts#create"
+    get "posts/:id" => "posts#show"
+    get "posts/:id/edit" => "posts#edit"
+    post "posts/:id/update" => "posts#update"
+    post "posts/:id/destroy" => "posts#destroy"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
